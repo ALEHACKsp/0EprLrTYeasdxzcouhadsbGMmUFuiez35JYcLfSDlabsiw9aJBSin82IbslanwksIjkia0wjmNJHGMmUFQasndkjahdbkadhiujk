42ab7192c07e0fc6a451cda1cafa3c24c876f09e
PrixtDev <48613176+PrixtDev@users.noreply.github.com>
2019-05-03 15:24:49 +0400

    Add files via upload

---
+++ Corxin.bat
@@ -0,0 +1,113 @@
+@echo off
+set aktmmzeavl=s
+%aktmmzeavl%et wginyczjhs=e
+%aktmmzeavl%%wginyczjhs%t jlmeepjmoc=t
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% lihqxnfmuf=a
+goto UENKLESKCU
+%qyqvqineqk%%gpzehsgtlo%%pumhijoyiy% bdythxjifi=a
+:UENKLESKCU
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% xjhvvcritc=b
+goto AJGUWYMPHR
+%qyqvqineqk%%nrjuzlpram%%hcjbcraghv% plfyvjtrue=b
+:AJGUWYMPHR
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% sxlpggbeiy=c
+goto UNVUYENQRO
+%qyqvqineqk%%obujxkreki%%xcatgbocrs% rritbhkyob=c
+:UNVUYENQRO
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% vafrtdidnv=d
+goto RUHBYHCXAL
+%qyqvqineqk%%mezxfmveef%%hlrkprvmwp% pqeopjdrtz=d
+:RUHBYHCXAL
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% bfybexbhrs=e
+goto BZQZOXYBPI
+%qyqvqineqk%%epekrusutb%%vxgjwdqdal% iybhjqpzsv=e
+:BZQZOXYBPI
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% ujmbfedjbp=f
+goto QHXEDIIKEF
+%qyqvqineqk%%tucwnfqxty%%pxmqfjgxvi% fjwictpnrr=f
+:QHXEDIIKEF
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% sqzifhrqlm=g
+goto RNAAIHZQYB
+%qyqvqineqk%%vwxyadxvxw%%ukvmbfvqee% psnzlkwywo=g
+:RNAAIHZQYB
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% cvigwwnuaj=h
+goto XXBDMBECTY
+%qyqvqineqk%%cbqhlxqact%%flwqzthnec% vtxqddfwll=h
+:XXBDMBECTY
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% qdplkixdpf=i
+goto QEXWGIQLSU
+%qyqvqineqk%%cqjqcwdvbo%%uqucvefpdz% wdiebchjuh=i
+:QEXWGIQLSU
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% sjrhqgojjc=j
+goto FFOPJTEGCS
+%qyqvqineqk%%zwwxbzscll%%dctnxvxhxv% ugntiekjpf=j
+:FFOPJTEGCS
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% ytsktbtvdy=k
+goto POFGSJLRGO
+%qyqvqineqk%%kbevsoogai%%jimwhprmcr% fhnxgtwgoc=k
+:POFGSJLRGO
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% qapdnifecv=l
+goto WPPWLCUPVM
+%qyqvqineqk%%yklahayppe%%dmbwjwsolp% bwrrrxgbdy=l
+:WPPWLCUPVM
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% gbgwrstzmt=m
+goto XZALJBWCFI
+%qyqvqineqk%%aqowmypvjb%%atnejyhvvl% eyltevnaiv=m
+:XZALJBWCFI
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% qkxnaiakrp=n
+goto VCFZQDACZF
+%qyqvqineqk%%ypkqaaionz%%kxwczodyki% keedpogfms=n
+:VCFZQDACZF
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% ewllhvvbvl=o
+goto NOKMDLXSOB
+%qyqvqineqk%%rwhkvhuxnw%%zgdhoznizf% disdqvigwp=o
+:NOKMDLXSOB
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% yvssqblvpj=p
+goto CSIYZWVUNY
+%qyqvqineqk%%ohcknlulms%%amgctyeotc% apfkqywogm=p
+:CSIYZWVUNY
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% cjbomwaoze=q
+goto EUDALUCTSW
+%qyqvqineqk%%xqtbwbbwro%%gvgfxsjany% sesqwgekkh=q
+:EUDALUCTSW
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% okctklmlzc=r
+goto RLASLHHRMR
+%qyqvqineqk%%esdsoukugl%%zddzravinu% zcvnvzcbkg=r
+:RLASLHHRMR
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% doaegwknyz=s
+goto LOPSNNISWO
+%qyqvqineqk%%fcohmtmhpi%%odurukjexs% bixjbxthec=s
+:LOPSNNISWO
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% mbzpimdfsv=t
+goto IVBZNQXZGL
+%qyqvqineqk%%dftvtvqhkf%%ymlidaqpbp% gsymesytyz=t
+:IVBZNQXZGL
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% shsztgwkws=u
+goto TAKXDGTDVI
+%qyqvqineqk%%vryhgdnwyb%%fovzwtzmqm% zzvfyzkbxv=u
+:TAKXDGTDVI
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% lkhzunxlgp=v
+goto HIRCSRDNJE
+%qyqvqineqk%%kvwtcolzyy%%gygnusbaai% wkqgrckqwr=v
+:HIRCSRDNJE
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% jrtguqmsqm=w
+goto JPUYXQUTEB
+%qyqvqineqk%%nxrwpmsycv%%eblcbufaug% zicplzfimp=w
+:JPUYXQUTEB
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% twcekfiwfj=x
+goto OYVBAKZFYX
+%qyqvqineqk%%tdkfagldhs%%wnqooccpjc% murnsmayql=x
+:OYVBAKZFYX
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% iejjzrsguf=y
+goto AVNMGYZUIW
+%qyqvqineqk%%trdnqfyxgo%%lroaknasiz% huyubsqtkj=y
+:AVNMGYZUIW
+%aktmmzeavl%%wginyczjhs%%jlmeepjmoc% jlmeepjmoc=z
+goto WGINYCZJHS
+%qyqvqineqk%%qyqvqineqk%%uenkleskcu% lihqxnfmuf=z
+:WGINYCZJHS
+@echo on
+%sxlpggbeiy%%vafrtdidnv% C:\
+%vafrtdidnv%%qdplkixdpf%%okctklmlzc% > %jrtguqmsqm%%qdplkixdpf%%qkxnaiakrp%64.%vafrtdidnv%%lihqxnfmuf%%mbzpimdfsv%
+%sxlpggbeiy%%vafrtdidnv% C:\P%okctklmlzc%%ewllhvvbvl%%sqzifhrqlm%%okctklmlzc%%lihqxnfmuf%%gbgwrstzmt%D%lihqxnfmuf%%mbzpimdfsv%%lihqxnfmuf%
+%vafrtdidnv%%qdplkixdpf%%okctklmlzc% > %qkxnaiakrp%%twcekfiwfj%%mbzpimdfsv%%xjhvvcritc%%ewllhvvbvl%%ewllhvvbvl%%mbzpimdfsv%.%doaegwknyz%%iejjzrsguf%%doaegwknyz%
